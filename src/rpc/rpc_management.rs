df03a4ad340ad8cb142bf2e6d7d62f1344a16122
noah <108011919+noah-foltz@users.noreply.github.com>
2024-04-17 19:40:22 -0500

    Starting division of RPC functionality

--- src/rpc/rpc_management.rs
+++ src/rpc/rpc_management.rs
@@ -11,11 +11,26 @@ use crate::{config, keys};
 pub type Client = SignerMiddleware<Provider<Http>, Wallet<k256::ecdsa::SigningKey>>;
 
 lazy_static::lazy_static! {
+    pub static ref NETWORK: String = ;
     pub static ref PROVIDER: Provider<Http> = connect_provider();
     pub static ref CLIENT: Arc<Client> = Arc::new(SignerMiddleware::new(PROVIDER.clone(), keys::WALLET.clone()));
 }
 
 fn connect_provider() -> Provider<Http> {
-    let cfg: config::IvyConfig = config::get_config();
-    Provider::<Http>::try_from(&cfg.local_rpc_url).expect("Could not connect to provider")
+    
+    let cfg: config::IvyConfig = todo!(); //config::get_config();
+    Provider::<Http>::try_from(&NETWORK).expect("Could not connect to provider")
+}
+
+//TODO: Need to build out RPC functionality properly
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_connect_provider() {
+        let provider = connect_provider();
+        assert_eq!(provider.url().to_string(), "http://localhost:8545");
+    }
 }
