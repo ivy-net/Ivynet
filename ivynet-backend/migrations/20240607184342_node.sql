7d157c48d24293e8d1e4560c7b38212bd09b04a9
Bazyli Zygan <bazyl@novaproject.net>
2024-07-09 11:22:49 +0200

    Because precommits prentend to be smarter than me

--- ivynet-backend/migrations/20240607184342_node.sql
+++ ivynet-backend/migrations/20240607184342_node.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS node (
     node_id            BYTEA     PRIMARY KEY,
-    organization_id    BIGINT    NOT NULL REFERENCES organization 
+    organization_id    BIGINT    NOT NULL REFERENCES organization
                                     ON DELETE CASCADE,
     created_at         TIMESTAMP NOT NULL,
     updated_at         TIMESTAMP NOT NULL
