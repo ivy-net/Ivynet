7eb25324c2993106bc1af600507daa6b3faacfdc
ivynet-phil <phil@ivynet.dev>
2024-08-27 12:23:33 -0500

    avs trait cleanup + keyfile

---
+++ core/abi/witness/README.md
@@ -0,0 +1,42 @@
+WITNESS CONTRACTS NOTES
+
+CONSTANTS
+```go
+var BlueOrangutan = ChainConfig{
+	OperatorRegistryAddress:      common.HexToAddress("0x26710e60A36Ace8A44e1C3D7B33dc8B80eAb6cb7"),
+	DiligenceProofManagerAddress: common.HexToAddress("0x7AB3b14F3177935d4539d80289906633615393F2"),
+	ChainID:                      *big.NewInt(1237146866),
+	BlockExplorer:                "https://blue-orangutan-blockscout.eu-north-2.gateway.fm",
+}
+
+var Holesky = ChainConfig{
+	OperatorRegistryAddress: common.HexToAddress("0x708CBDDdab358c1fa8efB82c75bB4a116F316Def"),
+	WitnessHubAddress:       common.HexToAddress("0xa987EC494b13b21A8a124F8Ac03c9F530648C87D"),
+	AVSDirectoryAddress:     common.HexToAddress("0x055733000064333CaDDbC92763c58BF0192fFeBf"),
+	ChainID:                 *big.NewInt(17000),
+	BlockExplorer:           "https://holesky.etherscan.io",
+}
+
+var WitnesschainMainnet = ChainConfig{
+	OperatorRegistryAddress:      common.HexToAddress("0xd11e55b821aC8509D2C17f5f76193351252d69aE"),
+	DiligenceProofManagerAddress: common.HexToAddress("0x7AB3b14F3177935d4539d80289906633615393F2"),
+	BlockExplorer:                "https://explorer.witnesschain.com",
+	ChainID:                      *big.NewInt(1702448187),
+}
+
+var EthMainnet = ChainConfig{
+	OperatorRegistryAddress: common.HexToAddress("0xef1a89841fd189ba28e780a977ca70eb1a5e985d"),
+	WitnessHubAddress:       common.HexToAddress("0xD25c2c5802198CB8541987b73A8db4c9BCaE5cC7"),
+	AVSDirectoryAddress:     common.HexToAddress("0x135dda560e946695d6f155dacafc6f1f25c1f5af"),
+	ChainID:                 *big.NewInt(1),
+	BlockExplorer:           "https://etherscan.io",
+}
+```
+
+### Operator Registry
+https://github.com/witnesschain-com/diligencewatchtower-contracts/blob/main/src/core/OperatorRegistry.sol
+
+Implements:
+- IOperatorRegistry
+- IDelegationManager
+- ISlasher
---
+++ core/src/keys/keyfile.rs
@@ -0,0 +1,71 @@
+use ethers::types::Address;
+use serde::{Deserialize, Serialize};
+
+use crate::wallet::IvyWallet;
+use std::{env, path::PathBuf};
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+pub struct EcdsaKeyfile {
+    pub path: PathBuf,
+    pub address: Address,
+    pub name: String,
+    pub pw_env_var: String,
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum KeyfileError {
+    #[error("No keyfile env var found: {0}")]
+    KeyfileEnvVarError(String),
+    #[error("Could not decrypt keyfile: {0}")]
+    KeyfileDecryptionError(String),
+    #[error(transparent)]
+    VarError(#[from] std::env::VarError),
+    #[error(transparent)]
+    DialoguerError(#[from] dialoguer::Error),
+    #[error("Environment variable {0} not found for keyname {1}. Ensure the variable is set or in a .env file.")]
+    EnvVarError(String, String),
+}
+
+impl EcdsaKeyfile {
+    pub fn new(path: PathBuf, address: Address, name: &str, pw_env_var: &str) -> Self {
+        EcdsaKeyfile { path, address, name: name.to_owned(), pw_env_var: pw_env_var.to_owned() }
+    }
+    pub fn decrypt(&self, password: String) -> Result<IvyWallet, KeyfileError> {
+        IvyWallet::from_keystore(self.path.clone(), &password)
+            .map_err(|_| KeyfileError::KeyfileDecryptionError(self.path.display().to_string()))
+    }
+    pub fn decrypt_env(&self) -> Result<IvyWallet, KeyfileError> {
+        // check if the env var is set
+        let password = match env::var(&self.pw_env_var) {
+            Ok(pw) => pw,
+            Err(_) => {
+                // Attempt to load from .env if not present
+                dotenvy::var(&self.pw_env_var).map_err(|_| {
+                    KeyfileError::EnvVarError(self.name.clone(), self.pw_env_var.clone())
+                })?
+            }
+        };
+        self.decrypt(password)
+    }
+}
+
+pub fn prompt_ecdsa_keyfile() -> Result<EcdsaKeyfile, KeyfileError> {
+    let path = dialoguer::Input::<String>::new()
+        .with_prompt("Enter the full path to the keyfile")
+        .interact()?;
+    let name =
+        dialoguer::Input::<String>::new().with_prompt("Enter the keyfile name.").interact()?;
+    let pw_env_var = dialoguer::Input::<String>::new()
+        .with_prompt("Enter the password environment variable name. This will be used later to decrypt the keyfile.")
+        .interact()?;
+
+    // Key validation
+    let pw_value = std::env::var(&pw_env_var)?;
+    let wallet = IvyWallet::from_keystore(path.clone().into(), &pw_value)
+        .map_err(|_| KeyfileError::KeyfileDecryptionError(path.clone()))?;
+
+    // Derive address for later
+    let address = wallet.address();
+
+    Ok(EcdsaKeyfile::new(path.into(), address, &name, &pw_env_var))
+}
