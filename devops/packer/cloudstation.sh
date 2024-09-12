1de35c6bbab3fff844643ebddfa5a0cd97733d89
Wawrzyniec 'Wawrzek' Niewodniczański <wawrzek@ivynet.dev>
2024-09-12 11:31:16 +0100

    Better scripts to run packer

--- devops/packer/cloudstation.sh
+++ devops/packer/cloudstation.sh
@@ -3,9 +3,16 @@
 # Simple script to create backend image
 # Requires toml cargo package
 
+filename=cloudstation.pkr.hcl
+
+echo "Get verions (tag)"
 version=$(toml get ../../cli/Cargo.toml package.version |tr -d [\"\.])
 
-
+echo "Activate Ansible"
 source $HOME/bin/ansible/bin/activate
 
-packer build -var "version=${version}" cloudstation.pkr.hcl
+echo "Initialize packer"
+packer init
+
+echo "Run packer"
+packer build -var "version=${version}" ${filename}
