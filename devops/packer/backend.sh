2559317b842e7d3b7bcfe081c016ff09a282ab1c
Wawrzyniec 'Wawrzek' Niewodniczański <wawrzek@ivynet.dev>
2024-09-12 14:28:13 +0100

    [Devops] better workflows  (#151)
    Create one 'build' workflow to run on merge. The workflow
    -- builds binaries
    -- creates git tags based on client and backend
    
    Also in the PR:
    -- add or improve shell scripts to run packer (to build cloud images)
    -- add a script to run Ansible (to upgrade current test backend)
    -- some other cosmetic changes

--- devops/packer/backend.sh
+++ devops/packer/backend.sh
@@ -3,9 +3,16 @@
 # Simple script to create backend image
 # Requires toml cargo package
 
+filename=backend.pkr.hcl
+
+echo "Get verions (tag)"
 version=$(toml get ../../backend/Cargo.toml package.version |tr -d [\"\.])
 
-
+echo "Activate Ansible"
 source $HOME/bin/ansible/bin/activate
 
-packer build -var "version=${version}" backend.pkr.hcl
+echo "Initialize packer"
+packer init
+
+echo "Run packer"
+packer build -var "version=${version}" ${filename}
