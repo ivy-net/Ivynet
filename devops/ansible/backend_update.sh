047a8a1dbd0e97918ee14358709dba217f77b888
Wawrzyniec 'Wawrzek' Niewodniczański <wawrzek@ivynet.dev>
2024-09-18 12:16:21 +0100

    Action to update test server
    GHA:
    - new workflow to run Ansible
    - test above workflow
    - cosmetic change in client workflow
    
    Ansible:
    - Adjust backend script to source local ansible only if required
    - Use dynamic inventory
    Packer:
    - Make the script for backend similar to the Ansible one
    A#

--- devops/ansible/backend_update.sh
+++ devops/ansible/backend_update.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 #
 # The script to run ansible to update current backend
+# Requires toml cargo package
 
 echo "Find the backend version in Cargo.toml"
 version=$(toml get ../../backend/Cargo.toml package.version |tr -d [\"])
 
-echo "Activate Ansible"
-source $HOME/bin/ansible/bin/activate
+echo "Activate Ansible (if necessary)"
+[ -f "$HOME/bin/ansible/bin/activate" ] && source $HOME/bin/ansible/bin/activate
 
 echo "Run the playbook"
-echo "ansible-playbook -i inventory backend.yml --extra-vars ivynet_backend_release=${version} --vault-password-file ~/.vault.txt"
-ansible-playbook -i inventory backend.yml --extra-vars ivynet_backend_release=${version} --vault-password-file ~/.vault.txt
+echo "ansible-playbook -i inventory gcp.yml --extra-vars ivynet_backend_release=${version} --vault-password-file ~/.vault.txt"
+ansible-playbook -i inventory gcp.yml --extra-vars ivynet_backend_release=${version} --vault-password-file ~/.vault.txt
