25e60e85cbdb4724a6d1260318e9a4fcc79f2972
Wawrzyniec 'Wawrzek' Niewodniczański <wawrzek@ivynet.dev>
2024-09-18 16:08:05 +0100

    Fix backend script

--- devops/ansible/backend_update.sh
+++ devops/ansible/backend_update.sh
@@ -3,12 +3,14 @@
 # The script to run ansible to update current backend
 # Requires toml cargo package
 
+remote_user=wawrzek_ivynet_dev
+
 echo "Find the backend version in Cargo.toml"
 version=$(toml get ../../backend/Cargo.toml package.version |tr -d [\"])
-
+echo "Version is: ${version}"
 echo "Activate Ansible (if necessary)"
 [ -f "$HOME/bin/ansible/bin/activate" ] && source $HOME/bin/ansible/bin/activate
 
 echo "Run the playbook"
 echo "ansible-playbook -i gcp.yml --extra-vars ivynet_backend_release=${version} --vault-password-file ~/.vault.txt" backend.yml
-ansible-playbook -i gcp.yml --extra-vars ivynet_backend_release=${version} --vault-password-file ~/.vault.txt
+ansible-playbook -i gcp.yml --extra-vars "ivynet_backend_release=${version} ansible_user=${remote_user}" --vault-password-file ~/.vault.txt backend.yml
