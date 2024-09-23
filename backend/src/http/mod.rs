85fba1597a1fc341868a5009a59b66b2f780f25a
Bazyli Zygan <bazyl@novaproject.net>
2024-09-23 17:13:35 +0200

    Fixing single machine lookup
    Version kick

--- backend/src/http/mod.rs
+++ backend/src/http/mod.rs
@@ -80,19 +80,19 @@ fn create_router() -> Router<HttpState> {
         .route("/health", get(|| async { "alive" }))
         .route("/authorize", options(handle_options))
         .route("/authorize", post(authorize::authorize))
-        .route("/authorize/invitation/{id}", get(authorize::check_invitation))
+        .route("/authorize/invitation/:id", get(authorize::check_invitation))
         .route("/authorize/forgot_password", post(authorize::forgot_password))
         .route("/authorize/set_password", post(authorize::set_password))
         .route("/organization", post(organization::new))
-        .route("/organization/{id}", get(organization::get))
+        .route("/organization/:id", get(organization::get))
         .route("/organization/invite", post(organization::invite))
-        .route("/organization/confirm/{id}", get(organization::confirm))
+        .route("/organization/confirm/:id", get(organization::confirm))
         .route("/organization/nodes", get(organization::nodes))
-        .route("/organization/nodes/{id}/metrics", get(organization::metrics))
+        .route("/organization/nodes/:id/metrics", get(organization::metrics))
         .route("/client/status", get(client::status))
         .route("/client/idle", get(client::idling))
         .route("/client/unhealthy", get(client::unhealthy))
-        .route("/client/info/{id}", get(client::info))
+        .route("/client/info/:id", get(client::info))
         .merge(
             SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", apidoc::ApiDoc::openapi()),
         )
@@ -117,8 +117,8 @@ async fn check_origin(mut request: Request, next: Next) -> Response {
             debug!("URL: {:#?}", url);
             debug!("Domain: {:#?}", url.domain());
             debug!("Scheme: {:#?}", url.scheme());
-            url.scheme() == "https" &&
-                url.domain().map_or(false, |domain| {
+            url.scheme() == "https"
+                && url.domain().map_or(false, |domain| {
                     domain == "ivynet.dev" || domain.ends_with(".ivynet.dev")
                 })
         })
