a7afdaa7434c259620a9e30527c9d767460bb54c
Wawrzyniec 'Wawrzek' Niewodniczański <wawrzek@ivynet.dev>
2024-09-24 09:56:41 +0100

    Add LB for GRPC (port 50050)

--- devops/terraform/vm+lb/lb.tf
+++ devops/terraform/vm+lb/lb.tf
@@ -1,5 +1,5 @@
 resource "google_compute_health_check" "backend" {
-  name               = "backend-basic-check"
+  name               = "backend-http-check"
   check_interval_sec = 5
   healthy_threshold  = 2
   http_health_check {
@@ -12,8 +12,8 @@ resource "google_compute_health_check" "backend" {
   unhealthy_threshold = 2
 }
 
-resource "google_compute_backend_service" "backend" {
-  name                            = "ivynet-backend-service"
+resource "google_compute_backend_service" "http" {
+  name                            = "ivynet-http-service"
   connection_draining_timeout_sec = 0
   health_checks                   = [google_compute_health_check.backend.id]
   load_balancing_scheme           = "EXTERNAL_MANAGED"
@@ -28,23 +28,59 @@ resource "google_compute_backend_service" "backend" {
   }
 }
 
+resource "google_compute_backend_service" "grpc" {
+  name                            = "ivynet-grpc-service"
+  connection_draining_timeout_sec = 0
+  health_checks                   = [google_compute_health_check.backend.id]
+  load_balancing_scheme           = "EXTERNAL_MANAGED"
+  port_name                       = "grpc"
+  protocol                        = "HTTP"
+  session_affinity                = "NONE"
+  timeout_sec                     = 30
+  backend {
+    group           = google_compute_instance_group.backend.id
+    balancing_mode  = "UTILIZATION"
+    capacity_scaler = 1.0
+  }
+}
+
 # This seems to be a name for loadbalacner
-resource "google_compute_url_map" "backend" {
+resource "google_compute_url_map" "http" {
   name            = "web-map-http"
-  default_service = google_compute_backend_service.backend.id
+  default_service = google_compute_backend_service.http.id
 }
 
-resource "google_compute_target_https_proxy" "backend" {
-  name             = "web-map-https"
+resource "google_compute_url_map" "grpc" {
+  name            = "web-map-grpc"
+  default_service = google_compute_backend_service.grpc.id
+}
+
+resource "google_compute_target_https_proxy" "http" {
+  name             = "web-map-http"
+  url_map          = google_compute_url_map.http.id
   ssl_certificates = [google_compute_managed_ssl_certificate.api.id]
-  url_map          = google_compute_url_map.backend.id
 }
 
-resource "google_compute_global_forwarding_rule" "backend" {
+resource "google_compute_target_https_proxy" "grpc" {
+  name             = "web-map-grpc"
+  url_map          = google_compute_url_map.grpc.id
+  ssl_certificates = [google_compute_managed_ssl_certificate.api.id]
+}
+
+resource "google_compute_global_forwarding_rule" "http" {
   name                  = "https-content-rule"
   ip_protocol           = "TCP"
   ip_address            = google_compute_global_address.loadbalancer.id
   load_balancing_scheme = "EXTERNAL_MANAGED"
   port_range            = "443"
-  target                = google_compute_target_https_proxy.backend.id
+  target                = google_compute_target_https_proxy.http.id
+}
+
+resource "google_compute_global_forwarding_rule" "grpc" {
+  name                  = "grpc-content-rule"
+  ip_protocol           = "TCP"
+  ip_address            = google_compute_global_address.loadbalancer.id
+  load_balancing_scheme = "EXTERNAL_MANAGED"
+  port_range            = "50050"
+  target                = google_compute_target_https_proxy.grpc.id
 }
