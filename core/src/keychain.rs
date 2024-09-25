74b90991ba9fa174915ccec2934127b79865773a
Bazyli Zygan <bazyl@novaproject.net>
2024-09-25 18:06:31 +0200

    Making BLS key work (#195)
    * Making BLS key work
    
    Removing oopsies
    
    * Version kick

--- core/src/keychain.rs
+++ core/src/keychain.rs
@@ -159,7 +159,7 @@ impl Keychain {
         }
 
         if keys.is_empty() {
-            return Err(IvyError::NoKeyFoundError)
+            return Err(IvyError::NoKeyFoundError);
         }
         let keys_display: &[String] = &keys;
 
@@ -222,6 +222,7 @@ impl Keychain {
             KeyName::Ecdsa(name) => format!("{name}.ecdsa.json"),
             KeyName::Bls(name) => format!("{name}.bls.json"),
         });
+        println!("Reading path {path:?}");
         let json = self.read_json_file(&path)?;
         let address = match json.get(match name {
             KeyName::Ecdsa(_) => "address",
