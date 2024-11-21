276803d6f9d30091836f7228a6e79fae9de9a026
noah <noah@ivynet.dev>
2024-11-21 13:25:32 -0600

    I ran fmt 3 times before the previous build failed...

--- backend/migrations/20241119200042_version_node_type.sql
+++ backend/migrations/20241119200042_version_node_type.sql
@@ -3,6 +3,6 @@
 ALTER TABLE avs_version_data RENAME COLUMN avs_name TO node_type;
 
 -- Update the unique constraint
-ALTER TABLE avs_version_data 
+ALTER TABLE avs_version_data
     DROP CONSTRAINT avs_version_data_avs_name_chain_key,
     ADD CONSTRAINT avs_version_data_node_type_chain_key UNIQUE (node_type, chain);
