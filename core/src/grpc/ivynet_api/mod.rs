22d72dd078bccdad35f0e0411150af1610fbde5b
ivy-phil <phil@ivynet.dev>
2024-12-04 16:40:39 -0600

    remove deprecated GRPC modules and clean clippy doc comments (#301)
    Co-authored-by: Wawrzyniec 'Wawrzek' Niewodniczański <wawrzek@ivynet.dev>

--- core/src/grpc/ivynet_api/mod.rs
+++
@@ -1,7 +0,0 @@
-/// Note on nomenclature: We use snake_case module names instead of properly scoped names
-/// (ivy_daemon.types) because the latter does not work with tonic::include_proto!(), which breaks
-/// on the period in the string and does not handle module renaming.
-
-pub mod ivy_daemon_operator {
-    tonic::include_proto!("operator");
-}
--- core/src/grpc/mod.rs
+++ core/src/grpc/mod.rs
@@ -1,5 +1,4 @@
 pub mod client;
-pub mod ivynet_api;
 pub mod server;
 
 pub mod backend {
