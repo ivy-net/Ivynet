1952c32fa28f4d2c116bc1e8c2bc2adb5b44c88c
ivynet-phil <phil@ivynet.dev>
2024-12-10 02:36:20 -0600

    fix fixture whitespace

--- backend/fixtures/avs_version_hashes.sql
+++ backend/fixtures/avs_version_hashes.sql
@@ -4,14 +4,12 @@ INSERT INTO avs_version_hash (avs_type, hash, version) VALUES ('eigenda', 'sha25
 INSERT INTO avs_version_hash (avs_type, hash, version) VALUES ('eigenda', 'sha256:523ad0cfe03b299305aab5b41ac8825780965226c116cdad31e4954c27e7f0a3', '0.8.3-rc.0');
 INSERT INTO avs_version_hash (avs_type, hash, version) VALUES ('eigenda', 'sha256:2da1ae1a863c77a0bd8abd40f12f8edd94af04afe77c8f03402760ebd187d2d1', '0.8.3-feat-docker-refactor-single-multistage-file-with-bake.1');
 INSERT INTO avs_version_hash (avs_type, hash, version) VALUES ('eigenda', 'sha256:e94c7a05214a7323c5f3c9f454ce8e85199ff7d738c2b4ca44af616284cc495f', 'release-0.1.1');
-
 INSERT INTO avs_version_hash (avs_type, hash, version) VALUES ('lagrange-state-committee', 'sha256:f9d9f3fb2e528b0941934925d13c41448d11fd312874b2a13f20574afdc20de7', 'develop');
 INSERT INTO avs_version_hash (avs_type, hash, version) VALUES ('lagrange-state-committee', 'sha256:cacfcfd445097e2680d24ff581413ebd25ee0ad20d26f6596ebac27d51ab8b04', 'seq');
 INSERT INTO avs_version_hash (avs_type, hash, version) VALUES ('lagrange-state-committee', 'sha256:124ec6dbb8d2f2af1bfadcf98d3c6c8ca38ad1f41240aa4b0a4d65e3464399fb', 'v0.1.2');
 INSERT INTO avs_version_hash (avs_type, hash, version) VALUES ('lagrange-state-committee', 'sha256:09751776b06a3dbf01f2a3f7ef4e7fb561bbd4ff16af52b60bd4520aff511d43', 'v0.4.0');
 INSERT INTO avs_version_hash (avs_type, hash, version) VALUES ('lagrange-state-committee', 'sha256:ba8d2f984aff84a37a1267b1d8a9f6ac8c15e893d0edb5ae256d36abdfb50cf4', 'v0.4.2');
 INSERT INTO avs_version_hash (avs_type, hash, version) VALUES ('lagrange-state-committee', 'sha256:3a2da54a5bc91b8ca3c673f6700f1627be618661447a06e7178d3ff6cccdf47f', 'v1');
-
 INSERT INTO avs_version_hash (avs_type, hash, version) VALUES ('ava-protocol', 'sha256:c45686b77ce2477278c2dcec16c93292e8809cf60cccbc8ab52c4474f429af24', 'e4cf8f3');
 INSERT INTO avs_version_hash (avs_type, hash, version) VALUES ('ava-protocol', 'sha256:984e77b994bf319f014c2c163fea33cb7983d23b2d41f220a4f8e02925355074', 'fa6335b');
 INSERT INTO avs_version_hash (avs_type, hash, version) VALUES ('ava-protocol', 'sha256:c45686b77ce2477278c2dcec16c93292e8809cf60cccbc8ab52c4474f429af24', 'main');
@@ -22,11 +20,8 @@ INSERT INTO avs_version_hash (avs_type, hash, version) VALUES ('ava-protocol', '
 INSERT INTO avs_version_hash (avs_type, hash, version) VALUES ('ava-protocol', 'sha256:0c5bf6731cc07c784296f184a909e55af7447ad7cf9eb17985c0812d109821f9', '4c2a01e');
 INSERT INTO avs_version_hash (avs_type, hash, version) VALUES ('ava-protocol', 'sha256:19e75e29cd66cad582a54c83e211e7768aa68f5a63793fd3a901b46359682d30', 'add-operator-docker');
 INSERT INTO avs_version_hash (avs_type, hash, version) VALUES ('ava-protocol', 'sha256:2da1ae1a863c77a0bd8abd40f12f8edd94af04afe77c8f03402760ebd187d2d1', '1.2.0-feat-prerelease');
-
-
 INSERT INTO avs_version_hash (avs_type, hash, version) VALUES ('k3-labs-avs', 'sha256:d09567b4681e27488878ca9b4727691be1b578935d3420a7d7a2c55b91955b3b', 'experimental');
 INSERT INTO avs_version_hash (avs_type, hash, version) VALUES ('k3-labs-avs', 'sha256:2a0bcaa6755ac7898901f898a1cbb2db7bd56e375cbe8943ac3b9929b3518613', 'latest');
-
 INSERT INTO avs_version_hash (avs_type, hash, version) VALUES ('lagrange-zk-worker-holesky', 'sha256:23ec9fb79730285054e21037a9c3b9d16b81f53ec381ca6e37fd757ca8c8a1e8', 'develop');
 INSERT INTO avs_version_hash (avs_type, hash, version) VALUES ('lagrange-zk-worker-holesky', 'sha256:8e9276e81975be0f7920f76d1123a4f2cb5a9b5985db482d3b96e245607ee71d', 'holesky');
 INSERT INTO avs_version_hash (avs_type, hash, version) VALUES ('lagrange-zk-worker-holesky', 'sha256:e3d800fafd1a97a1a424da3ba9e278003c6946948a7de8cfb8b8fe96933430f5', 'mainnet');
@@ -36,7 +31,6 @@ INSERT INTO avs_version_hash (avs_type, hash, version) VALUES ('lagrange-zk-work
 INSERT INTO avs_version_hash (avs_type, hash, version) VALUES ('lagrange-zk-worker-holesky', 'sha256:38d3e44685ab72830923320067f1d98175d69e227881689422ee4b82d2947fbe', 'v1.0.1');
 INSERT INTO avs_version_hash (avs_type, hash, version) VALUES ('lagrange-zk-worker-holesky', 'sha256:b01e1dc761729088821cd2b2656decf18c869827600fc38c3ea715a3df25f050', 'v1.1.0');
 INSERT INTO avs_version_hash (avs_type, hash, version) VALUES ('lagrange-zk-worker-holesky', 'sha256:91d149d9c6a032d0f9f204fe67ca0ea1e337cd39acfca41f1fd8a020acc5a706', 'v1_base');
-
 INSERT INTO avs_version_hash (avs_type, hash, version) VALUES ('eoracle', 'sha256:2da1ae1a863c77a0bd8abd40f4328edd94af04afe77c8f03402760ebd187d2d2', 'v0.1.7-arm64');
 INSERT INTO avs_version_hash (avs_type, hash, version) VALUES ('eoracle', 'sha256:2da1ae1a863c77a0bd8abd40f4328edd94af04afe77c8f03402760ebd187d2d3', 'v0.1.8'); 
 INSERT INTO avs_version_hash (avs_type, hash, version) VALUES ('eoracle', 'sha256:2da1ae1a863c77a0bd8abd40f4328edd94af04afe77c8f03402760ebd187d2d4', 'v0.1.8-amd64');   
