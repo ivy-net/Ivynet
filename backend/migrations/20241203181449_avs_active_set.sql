1172161795b633ad932a5041cd319756303519ce
ivynet-phil <phil@ivynet.dev>
2024-12-12 14:54:32 -0600

    clean up db migrations and rename files to underlying tables

--- backend/migrations/20241203181449_avs_active_set.sql
+++ backend/migrations/20241203181449_avs_active_set.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS avs_active_set (
-    directory BYTEA NOT NULL,
+    avs       BYTEA NOT NULL,
+    directory BYTEA NOT NULL DEFAULT decode('00', 'hex'),
     operator  BYTEA NOT NULL,
     chain_id  BIGINT NOT NULL,
     active    BOOL NOT NULL,
@@ -7,3 +8,5 @@ CREATE TABLE IF NOT EXISTS avs_active_set (
     log_index BIGINT NOT NULL,
     PRIMARY KEY ( directory, operator, chain_id )
 );
+
+CREATE INDEX avs_active_set_collection ON avs_active_set USING hash (directory);
--- backend/migrations/20241210125611_avs_active_set_collection.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE avs_active_set RENAME COLUMN directory TO avs;
-ALTER TABLE avs_active_set ADD COLUMN directory BYTEA NOT NULL DEFAULT decode('00', 'hex');
-
-CREATE INDEX avs_active_set_collection ON avs_active_set USING hash (directory);
