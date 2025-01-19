e2d809b89afb0bc39e38ce8852e8d2c9e6728114
noah <108011919+noah-foltz@users.noreply.github.com>
2025-01-19 16:27:15 -0600

    scraper versioning (#426)

--- scraper/src/main.rs
+++ scraper/src/main.rs
@@ -10,7 +10,12 @@ use scraper::blockchain;
 use tracing::{info, level_filters::LevelFilter};
 use tracing_subscriber::FmtSubscriber;
 
-#[derive(Clone, Parser)]
+mod version_hash {
+    include!(concat!(env!("OUT_DIR"), "/version.rs"));
+}
+
+#[derive(Clone, Parser, Debug)]
+#[command(name = "scraper", version = version_hash::VERSION_HASH, about = "Ivynet scraper service")]
 pub struct Params {
     #[arg(long, env = "BACKEND_URL", value_parser = Uri::from_str, default_value = if cfg!(debug_assertions) {
         "http://localhost:50051"
