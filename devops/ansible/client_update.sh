89067998036399dbe803670241402b366039c819
Wawrzyniec 'Wawrzek' Niewodniczański <wawrzek@ivynet.dev>
2025-01-23 15:49:13 +0000

    Improve running of the client update

--- devops/ansible/client_update.sh
+++ devops/ansible/client_update.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 #
 
+echo "Find the client version in Cargo.toml"
+version=$(toml get ../../cli/Cargo.toml package.version |tr -d [\"])
+echo " Version/Release is: ${version}"
+echo "Check if remote user is defined"
 remote_user="${remote_user:-wawrzek_ivynet_dev}"
 echo " Remote user is: ${remote_user}"
 
-echo "Find the backend version in Cargo.toml"
-version=$(toml get ../../cli/Cargo.toml package.version |tr -d [\"])
-echo "Version/Release is: ${version}"
 
-#  -e "ivynet_client_release=${version}" \
 ansible-playbook \
   -i gcp.yml \
   -u ${remote_user} \
   --vault-password-file ~/.vault.txt \
-  --tags github \
   ivynet_client.yml
