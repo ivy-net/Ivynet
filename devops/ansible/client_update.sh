e4dc47d7a271058dc2da0148bc3d6f02a43716e8
Wawrzyniec 'Wawrzek' Niewodniczański <wawrzek@ivynet.dev>
2025-01-31 14:39:48 +0000

    Sync update scripts

--- devops/ansible/client_update.sh
+++ devops/ansible/client_update.sh
@@ -1,16 +1,27 @@
 #!/bin/sh
 #
+# The script to run ansible to update current backend
+# Requires toml cargo package
 
-echo "Find the client version in Cargo.toml"
-version=$(toml get ../../cli/Cargo.toml package.version |tr -d [\"])
-echo " Version/Release is: ${version}"
 echo "Check if remote user is defined"
 remote_user="${remote_user:-wawrzek_ivynet_dev}"
 echo " Remote user is: ${remote_user}"
 
+echo "Find the client version in Cargo.toml"
+version=$(toml get ../../cli/Cargo.toml package.version |tr -d [\"])
+echo " Version/Release is: ${version}"
+echo "Activate Ansible (if necessary)"
+[ -f "$HOME/bin/ansible/bin/activate" ] && source $HOME/bin/ansible/bin/activate
 
-ansible-playbook \
-  -i gcp.yml \
-  -u ${remote_user} \
-  --vault-password-file ~/.vault.txt \
+cat << EOF
+Run the playbook
+
+
+ansible-playbook \\
+  -i gcp.yml \\
+  -u ${remote_user} \\
+  --vault-password-file ~/.vault.txt \\
   ivynet_client.yml
+EOF
+
+ansible-playbook -i gcp.yml -u ${remote_user} --vault-password-file ~/.vault.txt ivynet_client.yml
