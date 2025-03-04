aa97c0eccb5b94dd5bf057da790dc4c461b15ed8
noah <108011919+noah-foltz@users.noreply.github.com>
2025-03-04 12:21:12 -0600

    renaming some migration/notification bits

--- api/src/http/alerts.rs
+++ api/src/http/alerts.rs
@@ -5,8 +5,12 @@ use axum::{
 };
 use axum_extra::extract::CookieJar;
 use chrono::DateTime;
-use ivynet_database::alerts::{alerts_active::ActiveAlert, alerts_historical::HistoryAlert};
-use serde::Deserialize;
+use ivynet_database::{
+    alerts::{alerts_active::ActiveAlert, alerts_historical::HistoryAlert},
+    notification_settings::SettingsType,
+    Notifications, NotificationsSettings,
+};
+use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
 use uuid::Uuid;
 
@@ -28,6 +32,11 @@ pub struct AcknowledgeAlertParams {
     pub alert_id: Uuid,
 }
 
+/* --------------------------------
+----BASE ALERT FUNCTIONALITY-------
+----------------------------------- */
+
+/// Get all active alerts for every machine
 #[utoipa::path(
     get,
     path = "/alerts/active",
@@ -50,6 +59,7 @@ pub async fn active_alerts(
     Ok(Json(alerts))
 }
 
+/// Acknowledge an alert
 #[utoipa::path(
     post,
     path = "/alerts/acknowledge",
@@ -77,6 +87,7 @@ pub async fn acknowledge_alert(
     Ok(())
 }
 
+/// Get historical alerts for all machines
 #[utoipa::path(
     get,
     path = "/alerts/history",
@@ -107,3 +118,85 @@ pub async fn alert_history(
             .collect();
     Ok(Json(alerts))
 }
+
+/* ---------------------------------------
+-------SERVICE FLAG FUNCTIONALITY---------
+------------------------------------------ */
+
+#[derive(Serialize, Deserialize, Debug, Clone, ToSchema)]
+pub struct NotificationServiceSettings {
+    pub telegram: TelegramSettings,
+    pub email: EmailSettings,
+    pub pagerduty: PagerDutySettings,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, ToSchema)]
+pub struct TelegramSettings {
+    pub enabled: bool,
+    pub chats: Vec<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, ToSchema)]
+pub struct PagerDutySettings {
+    pub enabled: bool,
+    pub integration_key: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, ToSchema)]
+pub struct EmailSettings {
+    pub enabled: bool,
+    pub emails: Vec<String>,
+}
+
+impl From<(Notifications, Vec<NotificationsSettings>)> for NotificationServiceSettings {
+    fn from(value: (Notifications, Vec<NotificationsSettings>)) -> Self {
+        let mut emails = Vec::new();
+        let mut chats = Vec::new();
+        let mut integration_key = None;
+
+        for setting in value.1 {
+            match setting.settings_type {
+                SettingsType::Email => emails.push(setting.settings_value.clone()),
+                SettingsType::Telegram => chats.push(setting.settings_value.clone()),
+                SettingsType::PagerDuty => integration_key = Some(setting.settings_value.clone()),
+            }
+        }
+
+        Self {
+            email: EmailSettings { enabled: value.0.email, emails },
+            telegram: TelegramSettings { enabled: value.0.telegram, chats },
+            pagerduty: PagerDutySettings { enabled: value.0.pagerduty, integration_key },
+        }
+    }
+}
+
+/// Listing current service settings for notifications - Email, Telegram, PagerDuty
+#[utoipa::path(
+    get,
+    path = "/alerts/notifications/services",
+    responses(
+        (status = 200, body = NotificationSettings),
+        (status = 404)
+    )
+)]
+pub async fn get_notification_settings(
+    headers: HeaderMap,
+    State(state): State<HttpState>,
+    jar: CookieJar,
+) -> Result<Json<NotificationsSettings>, BackendError> {
+    let account = authorize::verify(&state.pool, &headers, &state.cache, &jar).await?;
+
+    let notifications =
+        Notifications::get(&state.pool, account.organization_id as u64).await.unwrap_or_default();
+    let not_settings =
+        Notifications::get_notification_settings(&state.pool, account.organization_id as u64, None)
+            .await?;
+
+    let response: NotificationsSettings = (notifications, not_settings).into();
+
+    Ok(response.into())
+}
+
+/* ---------------------------------------
+----NOTIFICATION FLAG FUNCTIONALITY-------
+------------------------------------------ */
