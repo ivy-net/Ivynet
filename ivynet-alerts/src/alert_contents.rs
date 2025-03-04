f34d0c19378fa12ba8fbb95b4ee011d17b9fa0ec
ivynet-phil <phil@ivynet.dev>
2025-03-04 11:52:54 -0600

    init

---
+++ ivynet-alerts/src/alert_contents.rs
@@ -0,0 +1,10 @@
+use ethers::types::Address;
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+#[derive(Debug, Clone, Serialize, Deserialize, Default, PartialEq, Eq)]
+pub struct Node {
+    pub node_name: String,
+    pub node_type: String,
+    pub machine_id: Uuid,
+}
