4b0d665ff703286d9e6ed034472efb0dcd99c965
Wawrzyniec 'Wawrzek' Niewodniczański <wawrzek@ivynet.dev>
2025-03-09 14:35:01 +0000

    Env for api

--- devops/ansible/roles/ivynet-api/files/ivynet-version-scanner.sh
+++ devops/ansible/roles/ivynet-api/files/ivynet-version-scanner.sh
@@ -2,7 +2,7 @@
 #
 IVYNET_PATH=/opt/ivynet
 
-source ${IVYNET_PATH}/secrets/env
+source ${IVYNET_PATH}/secrets/env.api
 
 ${IVYNET_PATH}/bin/ivynet-api --add-node-version-hashes --db-uri $DATABASE_URL
 ${IVYNET_PATH}/bin/ivynet-api --update-node-data-versions --db-uri $DATABASE_URL
