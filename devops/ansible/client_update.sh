c25fd8525cf591a60ffcf8bd47b749df432e9c05
Wawrzyniec 'Wawrzek' Niewodniczański <wawrzek@ivynet.dev>
2025-03-17 17:41:08 +0000

    [IV-252] reset chain (#551)
    * One script for both release and master
    
    * Adjust path
    
    * More vars + copy reference config
    
    * Extra script
    
    * Enable handling release and dev in one script
    
    script has the option to run with master
    
    * Fix path in files
    
    * Fix script name
    
    ---------
    
    Co-authored-by: noah <108011919+noah-foltz@users.noreply.github.com>

--- devops/ansible/client_update.sh
+++ devops/ansible/client_update.sh
@@ -3,6 +3,14 @@
 # The script to run ansible to update current client
 # Requires toml cargo package
 
+while getopts m flag
+do
+  case "${flag}" in
+    m) MASTER=True;;
+  esac
+done
+
+
 echo "Check if remote user is defined"
 remote_user="${remote_user:-wawrzek_ivynet_dev}"
 echo " Remote user is: ${remote_user}"
@@ -13,6 +21,28 @@ echo " Version/Release is: ${version}"
 echo "Activate Ansible (if necessary)"
 [ -f "$HOME/bin/ansible/bin/activate" ] && source $HOME/bin/ansible/bin/activate
 
+if [[ ${MASTER} == 'True' ]]
+then
+cat << EOF
+Run the playbook
+
+
+ansible-playbook \\
+  -i gcp.yml \\
+  -u ${remote_user} \\
+  -e "ivynet_client_is_release=false" \\
+  --vault-password-file ~/.vault.txt \\
+  ivynet-client.yml
+EOF
+  sed -i.bak 's/\(gcp_area_client\)/\1\:\&gcp_env_dev/' ivynet-client.yml
+  ansible-playbook \
+    -i gcp.yml \
+    -u ${remote_user} \
+    -e "ivynet_client_is_release=false" \
+    --vault-password-file ~/.vault.txt \
+    ivynet-client.yml
+  mv ivynet-client.yml.bak ivynet-client.yml
+else
 cat << EOF
 Run the playbook
 
@@ -23,5 +53,9 @@ ansible-playbook \\
   --vault-password-file ~/.vault.txt \\
   ivynet-client.yml
 EOF
-
-ansible-playbook -i gcp.yml -u ${remote_user} --vault-password-file ~/.vault.txt ivynet-client.yml
+  ansible-playbook \
+    -i gcp.yml \
+    -u ${remote_user} \
+    --vault-password-file ~/.vault.txt \
+    ivynet-client.yml
+fi
