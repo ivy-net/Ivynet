5bd12da233b8479b82eebfcf156dc716332a7865
ivynet-phil <phil@ivynet.dev>
2025-03-25 09:20:35 -0500

    move notification send check to post alert insert

--- ivynet-heartbeat/src/event.rs
+++ ivynet-heartbeat/src/event.rs
@@ -3,7 +3,7 @@ use std::sync::Arc;
 use chrono::{DateTime, Utc};
 use ivynet_alerts::AlertType;
 use ivynet_database::{error::DatabaseError, NotificationSettings};
-use ivynet_notifications::{Channel, NotificationDispatcher, OrganizationDatabase};
+use ivynet_notifications::{NotificationDispatcher, OrganizationDatabase};
 use sqlx::PgPool;
 
 use crate::{
@@ -39,56 +39,30 @@ impl<D: OrganizationDatabase> HeartbeatEventHandler<D> {
             HeartbeatEvent::NewNode(node_id) => self.handle_new_node(node_id).await?,
             HeartbeatEvent::StaleClient { client_id, last_heartbeat } => {
                 let settings = NotificationSettings::get_for_client(&self.db, client_id.0).await?;
-                if settings
-                    .alert_flags
-                    .is_alert_enabled(&AlertType::NoClientHeartbeat)
-                    .is_ok_and(|enabled| enabled)
-                {
-                    let channels = settings.get_active_channels();
-                    self.handle_stale_client(
-                        client_id,
-                        settings.organization_id,
-                        last_heartbeat,
-                        channels,
-                    )
-                    .await?
-                }
+                self.handle_stale_client(
+                    client_id,
+                    settings.organization_id,
+                    last_heartbeat,
+                    settings,
+                )
+                .await?
             }
             HeartbeatEvent::StaleMachine { machine_id, last_heartbeat } => {
                 let settings =
                     NotificationSettings::get_for_machine(&self.db, machine_id.0).await?;
-                if settings
-                    .alert_flags
-                    .is_alert_enabled(&AlertType::NoMachineHeartbeat)
-                    .is_ok_and(|enabled| enabled)
-                {
-                    let channels = settings.get_active_channels();
-                    self.handle_stale_machine(
-                        machine_id,
-                        settings.organization_id,
-                        last_heartbeat,
-                        channels,
-                    )
-                    .await?
-                }
+                self.handle_stale_machine(
+                    machine_id,
+                    settings.organization_id,
+                    last_heartbeat,
+                    settings,
+                )
+                .await?
             }
             HeartbeatEvent::StaleNode { node_id, last_heartbeat } => {
                 let settings =
                     NotificationSettings::get_for_machine(&self.db, node_id.machine).await?;
-                if settings
-                    .alert_flags
-                    .is_alert_enabled(&AlertType::NoNodeHeartbeat)
-                    .is_ok_and(|enabled| enabled)
-                {
-                    let channels = settings.get_active_channels();
-                    self.handle_stale_node(
-                        node_id,
-                        settings.organization_id,
-                        last_heartbeat,
-                        channels,
-                    )
+                self.handle_stale_node(node_id, settings.organization_id, last_heartbeat, settings)
                     .await?
-                }
             }
         }
         Ok(())
@@ -114,11 +88,18 @@ impl<D: OrganizationDatabase> HeartbeatEventHandler<D> {
         client_id: ClientId,
         organization_id: i64,
         last_response_time: DateTime<Utc>,
-        channels: Vec<Channel>,
+        settings: NotificationSettings,
     ) -> Result<(), HeartbeatError> {
         let alert = ClientHeartbeatAlert { client_id, last_response_time, created_at: Utc::now() };
         ClientHeartbeatAlert::insert(&self.db, alert.clone(), organization_id).await?;
-        self.notifier.notify(alert, channels).await?;
+        if settings
+            .alert_flags
+            .is_alert_enabled(&AlertType::NoMachineHeartbeat)
+            .is_ok_and(|enabled| enabled)
+        {
+            let channels = settings.get_active_channels();
+            self.notifier.notify(alert, channels).await?;
+        }
         Ok(())
     }
 
@@ -127,12 +108,19 @@ impl<D: OrganizationDatabase> HeartbeatEventHandler<D> {
         machine_id: MachineId,
         organization_id: i64,
         last_response_time: DateTime<Utc>,
-        channels: Vec<Channel>,
+        settings: NotificationSettings,
     ) -> Result<(), HeartbeatError> {
         let alert =
             MachineHeartbeatAlert { machine_id, last_response_time, created_at: Utc::now() };
         MachineHeartbeatAlert::insert(&self.db, alert.clone(), organization_id).await?;
-        self.notifier.notify(alert, channels).await?;
+        if settings
+            .alert_flags
+            .is_alert_enabled(&AlertType::NoMachineHeartbeat)
+            .is_ok_and(|enabled| enabled)
+        {
+            let channels = settings.get_active_channels();
+            self.notifier.notify(alert, channels).await?;
+        }
 
         Ok(())
     }
@@ -142,11 +130,18 @@ impl<D: OrganizationDatabase> HeartbeatEventHandler<D> {
         node_id: NodeId,
         organization_id: i64,
         last_response_time: DateTime<Utc>,
-        channels: Vec<Channel>,
+        settings: NotificationSettings,
     ) -> Result<(), HeartbeatError> {
         let alert = NodeHeartbeatAlert { node_id, last_response_time, created_at: Utc::now() };
         NodeHeartbeatAlert::insert(&self.db, alert.clone(), organization_id).await?;
-        self.notifier.notify(alert, channels).await?;
+        if settings
+            .alert_flags
+            .is_alert_enabled(&AlertType::NoMachineHeartbeat)
+            .is_ok_and(|enabled| enabled)
+        {
+            let channels = settings.get_active_channels();
+            self.notifier.notify(alert, channels).await?;
+        }
         Ok(())
     }
 }
